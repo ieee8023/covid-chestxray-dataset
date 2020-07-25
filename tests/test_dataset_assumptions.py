b727a0d0ba2008c6e3154c95d97757520b0fe600
bganglia <bganglia892@gmail.com>
2020-07-25 04:34:48 -0400

    test assumptions

---
+++ tests/test_dataset_assumptions.py
@@ -0,0 +1,19 @@
+import pytest
+import os
+import pandas as pd
+
+metadata = pd.read_csv("metadata.csv")
+
+def test_no_duplicate_images():
+    filenames = metadata["filename"]
+    print(filenames.value_counts())
+    assert len(set(filenames)) == len(filenames)
+
+def test_no_unlinked_images():
+    linked_images = metadata["filename"]
+    all_images = os.listdir("images")
+    assert set(all_images).difference(set(linked_images)) == set()
+
+def test_no_pediatric():
+    print(list(metadata["age"].unique()))
+    assert min(metadata["age"]) >= 18
