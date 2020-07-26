5c8451ced83d3202b20c63f5609ac8e5b775514b
bganglia <bganglia892@gmail.com>
2020-07-25 20:31:24 -0400

    fix broken test

--- tests/test_dataset_assumptions.py
+++ tests/test_dataset_assumptions.py
@@ -12,7 +12,7 @@ def test_no_unlinked_images():
     linked_images = metadata["filename"]
     all_images = os.listdir("images")
     #The only unlinked image should be the warning image
-    assert set(all_images).difference(set(linked_images)) == set('FAFA-A1BF-49A8-A1D3-66FAFA41B7345D.jpg')
+    assert set(all_images).difference(set(linked_images)) == set(['FAFA-A1BF-49A8-A1D3-66FAFA41B7345D.jpg'])
 
 def test_no_pediatric():
     assert min(metadata["age"]) >= 18
