f2bb5d3bda090fe6fa5430e52cdaa41c300f880e
bganglia <bganglia892@gmail.com>
2020-08-10 01:28:53 -0400

    Clean up cache browsing script

--- scripts/browse_page_from_cache.py
+++ scripts/browse_page_from_cache.py
@@ -12,17 +12,8 @@ def browse_page_from_cache(url):
     return browser
 
 if __name__ == "__main__":
-    #parser = argparse.ArgumentParser()
-    #parser.add_argument("url", default="")
-    #args = parser.parse_args()
-    #url = args.url
-    #if url == "":
-        url = input("Enter URL:")
-        while True:
-            browser = browse_page_from_cache(url)
-            url = input("Enter URL:")
-            browser.close()
-    #else:
-    #    browser = browse_page_from_cache(url)
-    #    input()
-    #    browser.close()
+    url = input("Enter URL: ")
+    while True:
+        browser = browse_page_from_cache(url)
+        url = input("Enter URL: ")
+        browser.close()
